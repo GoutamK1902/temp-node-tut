77692a5c5e5e5208af71cec82de5a8ba8dd66b1f
GoutamK1902 <goutamkhowal321@gmail.com>
2023-06-04 14:18:36 +0530

    ..

--- app.js
+++ app.js
@@ -1,21 +1,47 @@
-//  npm - global command , comes with node
-//  npm --version
+const { readFile, writeFile } = require("fs").promises;
+// const util = require("util");
+// const readFilePromise = util.promisify(readFile);
+// const writeFilePromise = util.promisify(writeFile);
 
-// local dependency - use it only in this particular project
-// npm i <packageName>
+const start = async () => {
+  try {
+    const first = await readFile("./content/first.txt", "utf8");
+    const second = await readFile("./content/second.txt", "utf8");
+    await writeFile(
+      "./content/result-mind-grenade.txt",
+      `This is amazing :  ${first} ${second} `,
+      { flag: "a" }
+    );
+    console.log(first, second);
+  } catch (error) {}
+};
 
-//global dependency -use it in any project
-//npm install -g <packageName>
-// sudo install -g <packageName> (mac)
+start();
 
-// manual approach (create package.json in the root, create properties etc)
-// npm init (step by step, press enter to skip)
-// npm init -y (everything default)
+// const getText = (path) => {
+//     return new Promise((resolve, reject) => {
+//       readFile(path, "utf8", (err, data) => {
+//         if (err) {
+//           reject(err);
+//         } else {
+//           resolve(data);
+//         }
+//       });
+//     });
+//   };
+// const start = async () => {
+//   try {
+//     const first = await getText("./content/first.txt");
+//     const second = await getText("./content/second.txt");
+//     console.log(first);
+//     console.log(second);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
 
-const _ = require("lodash");
+// start();
 
-const items = [1, [2, [3, [4]]]];
-
-const newItems = _.flattenDeep(items);
-console.log(newItems);
-console.log("Hare Krishna");
+// getText("./content/first.txt")
+//   .then((result) => console.log(result))
+//   .catch((err) => console.log(err));
